947fed9e6066a8cb9c435b008ea1eb83f92c6e0c
Artem Demidov <demidov@surfstudio.ru>
2022-03-05 16:16:03 +0300

    heroku deploy attempt

--- bot/bot.py
+++ bot/bot.py
@@ -4,11 +4,12 @@ import os
 from aiogram import Bot, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
 from aiogram.utils.executor import start_webhook
 
-from bot.settings import WEBAPP_HOST, WEBAPP_PORT, BOT_TOKEN, WEBHOOK_URL, WEBHOOK_PATH
-from bot.utils import convert_url
+from settings import WEBAPP_HOST, WEBAPP_PORT, BOT_TOKEN, WEBHOOK_URL, WEBHOOK_PATH
+from utils import convert_url
 
 
 bot = Bot(token=BOT_TOKEN)
@@ -16,28 +17,32 @@ dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
 
-@dp.message_handler(
-    regexp='((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([taobao]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*')
+@dp.message_handler(regexp='((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([taobao.com/]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*')
 async def echo_message(msg: types.Message):
+    print(msg)
     await msg.reply(convert_url(msg.text))
 
+if __name__ == '__main__':
+    logging.warning('started')
+    executor.start_polling(dp)
 
-async def on_startup(dp):
-    logging.warning('Starting connection. ')
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-
-
-async def on_shutdown(dp):
-    logging.warning('Bye! Shutting down webhook connection')
-
-
-def main():
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+#
+# async def on_startup(dp):
+#     logging.warning('Starting connection. ')
+#     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+#
+#
+# async def on_shutdown(dp):
+#     logging.warning('Bye! Shutting down webhook connection')
+#
+#
+# def main():
+#     logging.basicConfig(level=logging.INFO)
+#     start_webhook(
+#         dispatcher=dp,
+#         webhook_path=WEBHOOK_PATH,
+#         skip_updates=True,
+#         on_startup=on_startup,
+#         host=WEBAPP_HOST,
+#         port=WEBAPP_PORT,
+#     )
--- bot/settings.py
+++ bot/settings.py
@@ -16,4 +16,7 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = int(os.getenv('PORT'))
+WEBAPP_PORT = int(os.getenv('PORT', '3001'))
+
+# group settings
+CHAT_ID = os.getenv('CHAT_ID')
