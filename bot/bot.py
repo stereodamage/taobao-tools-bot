3d38d8d1d0bf62bd7d414324e9f2fb4db1647f7b
Artem Demidov <demidov@surfstudio.ru>
2022-03-05 15:44:33 +0300

    heroku deploy attempt

--- bot/bot.py
+++ bot/bot.py
@@ -2,21 +2,18 @@ import logging
 import os
 
 from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
 
 from aiogram.utils.executor import start_webhook
 
-from utils import convert_url
+from bot.settings import WEBAPP_HOST, WEBAPP_PORT, BOT_TOKEN, WEBHOOK_URL, WEBHOOK_PATH
+from bot.utils import convert_url
 
 
-WEBHOOK_HOST = ''
-WEBHOOK_PATH = ''
-WEBHOOK_URL = ''
-PROXY = 'http://proxy.server:3128'
-
-bot = Bot(token=os.getenv('TOKEN', ''), proxy=PROXY)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 
 @dp.message_handler(
@@ -32,32 +29,15 @@ async def on_startup(dp):
 
 async def on_shutdown(dp):
     logging.warning('Bye! Shutting down webhook connection')
-    await bot.delete_webhook(drop_pending_updates=True)
-
-    await dp.storage.close()
-    await dp.storage.wait_closed()
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    start_webhook(dispatcher=dp,
-                  webhook_path=WEBHOOK_PATH,
-                  skip_updates=True,
-                  on_startup=on_startup,
-                  on_shutdown=on_shutdown,
-                  host='0.0.0.0',
-                  port=3001,
-                  )
-    webhook = await bot.get_webhook_info()
-
-    # If URL is bad
-    if webhook.url != WEBHOOK_URL:
-        # If URL doesnt match current - remove webhook
-        if not webhook.url:
-            await bot.delete_webhook()
-    await bot.set_webhook(WEBHOOK_URL)
-
-
-if __name__ == '__main__':
-    main()
-
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
