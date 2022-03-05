a258999f3c882922c3bf75924de1c819a625b0a3
Artem Demidov <demidov@surfstudio.ru>
2022-03-05 15:08:41 +0300

    added webhook mode

--- bot/bot.py
+++ bot/bot.py
@@ -1,14 +1,19 @@
+import logging
 import os
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-import requests
+from aiogram.utils.executor import start_webhook
 
 from utils import convert_url
 
-bot = Bot(token=os.getenv('TOKEN', None))
+WEBHOOK_PATH = ''
+WEBHOOK_URL = ''
+PROXY = 'http://proxy.server:3128'
+
+bot = Bot(token=os.getenv('TOKEN', '5121542031:AAGGF2NgY3LiMOjZfvg9szIWIpVBkZDKCZc'))
 dp = Dispatcher(bot)
 
 
@@ -18,6 +23,38 @@ async def echo_message(msg: types.Message):
     await msg.reply(convert_url(msg.text))
 
 
+async def on_startup(dp):
+    logging.warning('Starting connection. ')
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dp):
+    logging.warning('Bye! Shutting down webhook connection')
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+
+async def classic_bot_start():
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    await dp.skip_updates()  # пропуск накопившихся апдейтов (необязательно)
+    await dp.start_polling()
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(dispatcher=dp,
+                  webhook_path=WEBHOOK_PATH,
+                  skip_updates=True,
+                  on_startup=on_startup,
+                  on_shutdown=on_shutdown,
+                  host='0.0.0.0',
+                  port=3001,
+                  )
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    main()
 
